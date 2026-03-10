eaa1bee5c00e461cadea031cd15cefb9dc46a495
shintaro ono <tennkuunoonoshinjal@gmail.com>
2026-03-11 00:01:08 +0900

    config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 /**
  * アプリケーション設定
  */
-window.API_BASE_URL = "https://pz2hoc7li0.execute-api.ap-northeast-1.amazonaws.com/dev";
+window.API_BASE_URL = "https://m78srmotvj.execute-api.ap-northeast-1.amazonaws.com/dev";
